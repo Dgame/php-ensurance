e690f4cd8147c68e6e04e6f922d47cc08d4072b5
Randy Schütt <rswhite4@googlemail.com>
2016-07-16 19:09:15 +0200

    New attempt of php-guard

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use function Dgame\Ensurance\enforce;
-
-require_once 'vendor/autoload.php';
-
-enforce('That must be a valid value of the given array')->ensure('Randy')->isString()->isValueOf(['Marcel']);
